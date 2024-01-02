7a688da353a7f1138e750f9c9c2eab01233c70ef
Pritam <84720825+pritam12426@users.noreply.github.com>
2024-01-02 07:09:36 +0530

    Added 2 function pathExist() && checkPath()
    checkPath() >> Take 2 Parameters as input and concatenate both strings
    into one string with name full path then after check that path is exist
    or not.
    
    pathExist() >> Take 1 parameter as input and check whether the path is
    exist in your local computer or not will return true if it is exist and
    written false if it is not exist.

--- src/main.c
+++ src/main.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
+#include <stdbool.h>
 #include <string.h>
+#include <sys/stat.h>
+
+
+bool pathExist(const char *_fullPath);
+void checkPath(const char *_fileDir, const char *_fileName);
+
 
 typedef struct {
 	char *fileName;
@@ -16,5 +21,33 @@ int main(void) {
 		#include "./pathData.txt"
 	};
 
+	unsigned long len = (sizeof(pathData) / sizeof(pathData[0]));
+
+	for (int i = 0; i < len; i++) {
+		;
+	}
+
 	return 0;
+}
+
+
+void checkPath(const char *_fileDir, const char *_fileName) {
+	unsigned long len_dir = strlen(_fileDir);
+	unsigned long len_file = strlen(_fileName);
+
+	char fullPath[(len_dir + len_file)];
+	sprintf(fullPath, "%s%s", _fileDir, _fileName);
+
+	if (pathExist(fullPath)) {
+		;
+	}
+
+	return ;
+}
+
+
+bool pathExist(const char *_fullPath) {
+	struct stat buffer;
+	if (stat(_fullPath, &buffer) == 0) return true;
+	return false;
 }
