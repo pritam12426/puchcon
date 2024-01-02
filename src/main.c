b9b466f8f463757eb1a6cd044841d2d273592837
Pritam <84720825+pritam12426@users.noreply.github.com>
2024-01-02 12:21:39 +0530

    Updated printlgo() function such that
    Will print the status of that file with a symbol with margin of -14
    strings then after the path of that file with the status, colour shown
    below.
    
    COPIED         -> file will Display log in Cyan.
    UPDATED        -> file will Display log in Green.
    UP TO DATE     -> file will Display log in Green.
    FILE NOT FOUND -> file will Display log in Green.

--- src/main.c
+++ src/main.c
@@ -2,24 +2,30 @@
 #include <string.h>
 #include <stdbool.h>
 #include <sys/stat.h>
-#include "./lib/user_function.c"
+#include "./lib/user_function.h"
 
 /* 
 * You need nerd font to render icons in terminal and as well as in your code editor.
 * For more info visit this side "https://www.nerdfonts.com/"
 */
 
-#define ICON_NO_CHANGE ""
-#define ICON_UPDATED   ""
-#define ICON_ALERT     "󰩋"
-#define ICON_ALERT     ""
-#define ICON_COPIED    ""
+#define ICON_COPIED     ""
+#define ICON_UPDATED    ""
+#define ICON_TYPE_DIR   "󱧴"
+#define ICON_NO_CHANGE  ""
+#define ICON_NOT_FOUND  "󰩋"
+#define ICON_OPEN_ERROR "󰀦"
 
 
-bool pathExist(const char *_fullPath);
-void printLog(const char *_fullPath, char _status);
+
+#define TERMINAL_SIZE terminal_col_size()
+#define STATUS_MARGIN 26
+
+
 void updateFile(const char *_source, const char *_target);
 void checkPath(const char *_fileDir, const char *_fileName);
+bool pathExist(char *_fullPath, unsigned long _string_len);
+void printLog(const char *_fullPath, char _status, unsigned long _string_len);
 
 
 typedef struct {
@@ -35,11 +41,16 @@ int main(void) {
 		#include "./pathData.txt"
 	};
 
-	unsigned long len = (sizeof(pathData) / sizeof(pathData[0]));
-
-	for (int i = 0; i < len; i++) {
-		;
-	}
+	unsigned long len = (sizeof(pathData) / sizeof(Filepath));
+	int i = 0;
+	// for (int i = 0; i < len; i++) {
+		printLog(pathData[i].systemDirectory, 'C', 55);
+		printLog(pathData[i].systemDirectory, 'A', 50);
+		printLog(pathData[i].systemDirectory, 'U', 50);
+		printLog(pathData[i].systemDirectory, 'N', 50);
+		printLog(pathData[i].systemDirectory, 'D', 50);
+		printLog(pathData[i].systemDirectory, 'O', 50);
+	// }
 
 	return 0;
 }
@@ -52,7 +63,7 @@ void checkPath(const char *_fileDir, const char *_fileName) {
 	char fullPath[(len_dir + len_file)];
 	sprintf(fullPath, "%s%s", _fileDir, _fileName);
 
-	if (pathExist(fullPath)) {
+	if (pathExist(fullPath, (len_dir + len_file - 1))) {
 		;
 	}
 
@@ -65,13 +76,15 @@ void updateFile(const char *_source, const char *_target) {
 
 	in = fopen(_source, "r");
 	if (in == NULL) {
-		/* TODO: write a fuction for printing log */
+		unsigned long  len = strlen(_source);
+		printLog(_source, 'O', len);
 		return ;
 	}
 
 	in = fopen(_target, "w");
 	if (out == NULL) {
-		/* TODO: write a fuction for printing log */
+		unsigned long  len = strlen(_source);
+		printLog(_source, 'O', len);
 		return ;
 	}
 
@@ -79,17 +92,70 @@ void updateFile(const char *_source, const char *_target) {
 	while ((ch = getc(in)) != EOF) {
 		putc(ch, out);
 	}
-
-	/* TODO: write a fuction for printing log */
 }
 
 
-bool pathExist(const char *_fullPath) {
+bool pathExist(char *_fullPath, unsigned long _string_len) {
 	struct stat buffer;
-	if (stat(_fullPath, &buffer) == 0) return true;
-	return false;
+	if (stat(_fullPath, &buffer) == 0) {
+		if (S_ISREG(buffer.st_mode)) {
+			return true;
+		}
+
+		else {
+			printLog(_fullPath, 'O', _string_len);
+			return false;
+		}
+	}
+
+	else {
+		printLog(_fullPath, 'O', _string_len);
+		return false;
+	}
 }
 
-void printLog(const char *_fullPath, char _status) {
-	;
+void printLog(const char *_fullPath, char _status, unsigned long _string_len) {
+	/* _status
+	 * char 'C' `copied`     == "File copied to you git repo"
+	 * char 'N' `no change`  == "Nothing is change in you local file"
+	 * char 'D' `OPEN ERROR` == "Past path is a directory, not a file"
+	 * char 'A' `alert`      == "File not found in you local directory"
+	 * char 'O' `OPEN ERROR` == "File can not be open for reading or writing"
+	 * char 'U' `updated`    == "File updated with you local file and git repo"
+	*/
+
+	unsigned short colorCode = 37;
+
+	if (_status == 'C') {
+		colorCode = 36;
+		printf("\033[1;%hum%s  %-*s\033[0m", colorCode, ICON_COPIED, (STATUS_MARGIN - 9), "COPIED");
+	}
+
+	else if (_status == 'O') {
+		colorCode = 33;
+		printf("\033[1;%hum%s  %-*s\033[0m", colorCode, ICON_OPEN_ERROR, (STATUS_MARGIN - 9),"OPEN ERROR");
+	}
+
+	else if (_status == 'D') {
+		colorCode = 91;
+		printf("\033[1;%hum%s  %-*s\033[0m", colorCode, ICON_TYPE_DIR, (STATUS_MARGIN - 9), "DIR ERRRO");
+	}
+
+	else if (_status == 'U') {
+		colorCode = 32;
+		printf("\033[1;%hum%s  %-*s\033[0m", colorCode, ICON_UPDATED, (STATUS_MARGIN - 9), "UPDATED");
+	}
+
+	else if (_status == 'A') {
+		colorCode = 101;
+		printf("\033[1;%hum%s  %-*s\033[0m", 31, ICON_NOT_FOUND, (STATUS_MARGIN - 9), "FILE NOT FOUND");
+	}
+
+	else {
+		printf("\033[1;%hum%s  %-*s\033[0m", colorCode, ICON_NO_CHANGE, (STATUS_MARGIN - 9), "UP TO DATE");
+	}
+
+	printf("\033[1;35m%s\033[0m", " | ");
+	printf("\033[0;%hum%-*s\033[0m", colorCode, (TERMINAL_SIZE - STATUS_MARGIN), _fullPath);
+	printf("\033[1;35m%s\033[0m\n", " | ");
 }
