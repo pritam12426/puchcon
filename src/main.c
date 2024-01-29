121b7e00249b1edc6811d78924e01329acfc886e
Pritam <84720825+pritam12426@users.noreply.github.com>
2024-01-29 21:09:59 +0530

    update: Add 2 func pushRepo && getCharWithoutEnter
    pushRepo() -> Function will check wether your local git reop is push
    to your remote git reop
    
    getCharWithoutEnter() -> As name says `get Char Without Enter` Will say
    user to enter a char value without entering the enter or return key
    on your keyboard.

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <termios.h>
 #include <sys/stat.h>
 #include "./lib/user_function.h"
 
@@ -20,12 +21,16 @@
 
 #define STATUS_MARGIN 17
 
+
 static int terminal_size = 0;
-static char *source_git_directory = NULL;
+static char *SOURCE_GIT_DIRECTORY = NULL;
 
 
-int checkCompEnv(void);
+int pushRepo(void);
+int  checkCompEnv(void);
+int  unChangeFiles(void);
 void makeDir(char *_child_dir);
+char getCharWithoutEnter(void);
 bool pathExist(char *_fullPath);
 void printLog(const char *_fullPath, char _status);
 void updateFile(const char *_source, const char *_target);
@@ -43,11 +48,11 @@ typedef struct {
 int main(void) {
 	/* writing data into Global variable */
 	terminal_size = terminal_col_size();
-	source_git_directory = getenv("COMP_GIT_SOURCE_DIR");
+	SOURCE_GIT_DIRECTORY = getenv("COMP_GIT_SOURCE_DIR");
 
 	if (checkCompEnv() != 0) return 1;
 
-	printf("\033[1;33m%s\033[0m: \033[1;31m%s\033[0m \033[1;37m%s\033[0m\n" , "Local Git Repo", "󰊢", source_git_directory);
+	printf("\033[1;33m%s\033[0m: \033[1;31m%s\033[0m \033[1;37m%s\033[0m\n" , "Local Git Repo", "󰊢", SOURCE_GIT_DIRECTORY);
 	printf("|-\033[1;35m%-*s\033[0m|-\033[1;34m%s\033[0m ", STATUS_MARGIN + 2, "STATUS", "FILES");
 	printNTime('-', terminal_size - 30, false);
 	printf("|\n");
@@ -73,12 +78,12 @@ int main(void) {
 			continue;
 		}
 
-		git_path_len += strlen(source_git_directory);
+		git_path_len += strlen(SOURCE_GIT_DIRECTORY);
 		git_path_len += strlen(pathData[i].gitDir);
 		git_path_len += strlen(pathData[i].newName);
 
 		char git_full_path[git_path_len + 1];
-		snprintf(git_full_path, sizeof(git_full_path), "%s%s%s", source_git_directory, pathData[i].gitDir, pathData[i].newName);
+		snprintf(git_full_path, sizeof(git_full_path), "%s%s%s", SOURCE_GIT_DIRECTORY, pathData[i].gitDir, pathData[i].newName);
 
 		if (pathExist(git_full_path)) {
 			if ( ! fileCmp(full_local_path, git_full_path)) {
@@ -99,6 +104,20 @@ int main(void) {
 	putchar('|');
 	printNTime('-', terminal_size - 2, false);
 	printf("|\n");
+
+	chdir(SOURCE_GIT_DIRECTORY);
+	if (unChangeFiles() != 0) {
+		return 1;
+	}
+
+	if (pushRepo() != 0) {
+		return 1;
+	}
+
+	// if () {
+		// TODO: Add a function for commit the file.
+	// }
+
 	return 0;
 }
 
@@ -149,12 +168,12 @@ bool pathExist(char *_fullPath) {
 
 void printLog(const char *_fullPath, char _status) {
 	/* _status
-	 * char 'C' `copied`     == "File copied to you git repo"
-	 * char 'N' `no change`  == "Nothing is change in you local file"
-	 * char 'D' `OPEN ERROR` == "Past path is a directory, not a file"
-	 * char 'A' `alert`      == "File not found in you local directory"
-	 * char 'O' `OPEN ERROR` == "File can not be open for reading or writing"
-	 * char 'U' `updated`    == "File updated with you local file and git repo"
+	 * char 'C' `COPIED`     => "File copied to you git repo"
+	 * char 'N' `NO CHANGE`  => "Nothing is change in you local file"
+	 * char 'D' `OPEN ERROR` => "Past path is a directory, not a file"
+	 * char 'A' `ALERT`      => "File not found in you local directory"
+	 * char 'O' `OPEN ERROR` => "File can not be open for reading or writing"
+	 * char 'U' `UPDATED`    => "File updated with you local file and git repo"
 	*/
 
 	unsigned short colorCode = 37;
@@ -199,7 +218,7 @@ void printLog(const char *_fullPath, char _status) {
 	printf("\033[0;%hum%-*s\033[0m", colorCode, (int)((terminal_size - STATUS_MARGIN - margin)), &_fullPath[address]);
 	printf("\033[1;35m%s\033[0m\n", " |");
 	fflush(stdout);
-	// usleep(100000);
+	usleep(10000);
 }
 
 
@@ -207,7 +226,6 @@ bool fileCmp(const char *_fullPath, const char *_fullPath2) {
 	FILE *file_local;
 
 	file_local = fopen(_fullPath, "r");
-
 
 	if (file_local == NULL) {
 		fclose(file_local);
@@ -247,7 +265,7 @@ void makeDir(char *_child_dir) {
 
 	char *pwd = getenv("PWD");
 
-	chdir(source_git_directory);
+	chdir(SOURCE_GIT_DIRECTORY);
 	char *ch = strtok(child_dir, "/");
 
 	while (ch != NULL) {
@@ -262,7 +280,7 @@ void makeDir(char *_child_dir) {
 
 
 int checkCompEnv(void) {
-	if (source_git_directory == NULL) {
+	if (SOURCE_GIT_DIRECTORY == NULL) {
 		const char *env_not_fond = {
 			#include "./error_mess/not_found_env.txt"
 		};
@@ -273,15 +291,15 @@ int checkCompEnv(void) {
 
 	struct stat stat_buffer;
 
-	int status = stat(source_git_directory, &stat_buffer);
+	int status = stat(SOURCE_GIT_DIRECTORY, &stat_buffer);
 
 	if (status == 0) {
 		if (S_ISDIR(stat_buffer.st_mode)) {
 			char *cwd = getenv("PWD");
-			chdir(source_git_directory);
+			chdir(SOURCE_GIT_DIRECTORY);
 
 			if (system("git rev-parse 2>> /dev/null") != 0) {
-				printf("\t \033[1;31m\033[0m  \033[1;33mENVIRONMENT VARIABLE\033[0m  \033[1;37mCOMP_GIT_SOURCE_DIR\033[0m `\033[1;101m%s\033[0m` is not a git repository!\n", source_git_directory);
+				printf("\t \033[1;31m\033[0m  \033[1;33mENVIRONMENT VARIABLE\033[0m  \033[1;37mCOMP_GIT_SOURCE_DIR\033[0m `\033[1;101m%s\033[0m` is not a git repository!\n", SOURCE_GIT_DIRECTORY);
 				chdir(cwd);
 				return 1;
 			}
@@ -296,11 +314,94 @@ int checkCompEnv(void) {
 			};
 
 			printf("\033[1;33mCOMP_GIT_SOURCE_DIR\033[0m `ENVIRONMENT VARIABLE`");
-			printf(" { \033[1;101m%s\033[0m } ", source_git_directory);
+			printf(" { \033[1;101m%s\033[0m } ", SOURCE_GIT_DIRECTORY);
 			printf("%s\n", env_not_dir);
 			return 1;
 		}
 	}
 
 	return 1;
+}
+
+
+int unChangeFiles(void) {
+	printf("\nFOUNDED CHANGES IN\n");
+	FILE *fp;
+	int ch = 0;
+
+	fp = popen("git status --porcelain", "r");
+
+	unsigned line = 1;
+	printf("%3d\033[1;35m: \033[0m", line++);
+
+	for (int i = 0; (ch = getc(fp)) != -1; i++) {
+		if (i < 3) {
+			continue;
+		}
+
+		if (ch == '\n') {
+			putchar('\n');
+			printf("%3d\033[1;35m: \033[0m", line++);
+			i = -1;
+			continue;
+		}
+
+		putchar(ch);
+	};
+
+	pclose(fp);
+	printf("\b\b\b\b\b");
+	return 0;
+}
+
+
+int pushRepo() {
+	FILE *fp = popen("git status | grep 'Your'", "r");
+	if (fp == NULL) {
+		perror("Error opening pipe");
+		return 1;
+	}
+
+	char buffer[31];
+	int isPushed = false;
+
+	if ((fgets(buffer, sizeof(buffer), fp) != NULL) && (strncasecmp(buffer, "Your branch is up to date with", 30)) == 0) isPushed = true;
+
+	pclose(fp);
+
+	if ( ! isPushed) {
+		printf("Repo is not push :: Hit [y/n] to push: ");
+
+		if (getCharWithoutEnter() == 'y') {
+			printf("\nTrying to push...\n");
+			if (system("git push origin main -f > /dev/null 2>&1") == 0) {
+				printf("Pushed Successful.\n");
+			}
+			else {
+				printf("Some thing wrong happened while pushing\n");
+				return 1;
+			}
+		}
+		else {
+			putchar('\n');
+		}
+	}
+
+	return 0;
+}
+
+char getCharWithoutEnter(void) {
+	char ch;
+	struct termios oldt, newt;
+
+	tcgetattr(STDIN_FILENO, &oldt);
+
+	newt = oldt;
+	newt.c_lflag &= ~(ICANON | ECHO);
+	tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+
+	ch = getchar();
+
+	tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+	return ch;
 }
